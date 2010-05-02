bf0877a9f304d99fadf2bb6f11bf684f98ca8b18
Salomon Sickert <sickert@in.tum.de>
2010-05-02 22:22:34 +0200

    Fix merge conflicts

--- nuts/sensors/twi.h
+++ nuts/sensors/twi.h
@@ -2,10 +2,33 @@
 #define TWI_H_INCLUDED
 
 #include <avr/io.h>
+<<<<<<< HEAD:nuts/sensors/twi.h
 
 void twi_start();
 
 void twi_stop();
+=======
+#include <stdbool.h>
+
+
+void twi_init(void);
+
+//void twi_set_mode(); //only master read/write
+
+enum twi_access_mode { READ=1, WRITE=0};
+
+void twi_start(void);
+
+void twi_connect(enum twi_access_mode, uint8_t);
+
+uint8_t twi_status(void);
+
+void twi_write(uint8_t);
+
+uint8_t twi_read(void);
+
+void twi_stop(void);
+>>>>>>> origin/nuts:nuts/sensors/twi.h
 
 #endif // TWI_H_INCLUDED
 
--- nuts/weatherstation/main.c
+++ nuts/weatherstation/main.c
@@ -1,8 +1,16 @@
 #include <../sensors/twi.h>
 
+<<<<<<< HEAD:nuts/weatherstation/main.c
 int main ()
+=======
+int main (void)
+>>>>>>> origin/nuts:nuts/weatherstation/main.c
 {
 	while(1)
 	{
 	}
 }
+<<<<<<< HEAD:nuts/weatherstation/main.c
+=======
+
+>>>>>>> origin/nuts:nuts/weatherstation/main.c
