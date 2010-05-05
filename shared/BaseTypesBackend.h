c871cf4ede5fffecb6cef76d146a5361363068fe
Salomon Sickert <sickert@in.tum.de>
2010-05-05 21:28:28 +0200

    Clean up, maybe fixed the "fifo" bug

--- shared/BaseTypesBackend.h
+++ shared/BaseTypesBackend.h
@@ -1,10 +1,11 @@
-
-
 #ifndef _BASETYPESBACKEND_H_
 #define _BASETYPESBACKEND_H_
 
+// Please update your files! This is gonna to be removed
 
-typedef unsigned char uint8_t;
+#include <stdint.h>
+
+/*typedef unsigned char uint8_t;
 typedef unsigned int uint16_t;
 typedef unsigned long uint32_t;
 
@@ -12,7 +13,7 @@ typedef char int8_t;
 
 #define __int8_t_defined
 typedef int int16_t;
-typedef long int32_t;
+typedef long int32_t;*/
 
 
 #endif /* _BASETYPESBACKEND_H_ */
