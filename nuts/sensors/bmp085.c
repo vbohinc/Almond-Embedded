9a251960fb0721fca22d07ad5fb40fe2ac4dae50
Linus Lotz <linus.lotz@mytum.de>
2010-05-07 22:47:50 +0200

    write conversion data to eeprom

--- nuts/sensors/bmp085.c
+++ nuts/sensors/bmp085.c
@@ -3,16 +3,18 @@
  * 
  */
 
-#include <twi/twi.h>
+#include <twi.h>
 #include <avr/eeprom.h>
 #include <stdint.h>
+
+#include "bmp085.h"
 
 struct temprature_conversion_data {
   uint16_t AC5;
   uint16_t AC6;
   int16_t MC;
   int16_t MD;
-}
+};
 
 struct pressure_conversion_data {
   int16_t AC1;
@@ -22,13 +24,13 @@ struct pressure_conversion_data {
   int16_t B1;
   int16_t B2;
   //int16_t MB; //not needed in any calculation so won't use it
-}
+};
 
 
 
 //the eeprom saved conversion data
-struct temprature_conversion_data EEMEM tempconv;
-struct press_conversion_data EEMEM pressconv;
+const struct temprature_conversion_data tempconv EEMEM;
+const struct pressure_conversion_data pressconv EEMEM;
 
 uint16_t get_word(const uint8_t word)
 {
@@ -46,11 +48,27 @@ uint16_t get_word(const uint8_t word)
   return result;
 }
 
-uint8_t bmp085_get_data(uint16_t * temprature, uint16_t * pressure)
+//uint8_t bmp085_get_data(uint16_t * temprature, uint16_t * pressure)
+//{
+//}
+
+void get_conversion_data(void)
 {
+  struct temprature_conversion_data temptable;
+  struct pressure_conversion_data presstable;
+  //temprature
+  temptable.AC5 = get_word(0xB2);
+  temptable.AC6 = get_word(0xB4);
+  temptable.MC  = get_word(0xBC);
+  temptable.MD  = get_word(0xBE);
+  eeprom_write_block(&temptable,&tempconv,sizeof(struct temprature_conversion_data));
+  //pressure
+  presstable.AC1 = get_word(0xAA);
+  presstable.AC2 = get_word(0xAC);
+  presstable.AC3 = get_word(0xAE);
+  presstable.AC4 = get_word(0xB0);
+  presstable.B1  = get_word(0xB6);
+  presstable.B2  = get_word(0xB8);
+  eeprom_write_block(&presstable,&pressconv,sizeof(struct pressure_conversion_data));
 }
 
-uint8_t get_conversion_data();
-{
-	
-}
