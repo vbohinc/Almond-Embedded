deb6083ce91282a27e90d0835fcfd458670b160a
Salomon Sickert <sickert@in.tum.de>
2010-05-19 20:12:18 +0200

    Initial Squirrel Playground

--- squirrel/squirrel.h
+++ squirrel/squirrel.h
@@ -1,14 +1,13 @@
 /*
 * Core libraries for squirrel
 */
-#ifndef SQUIRREL_H
-#define SQUIRREL_H
+
+#ifndef __SQUIRREL_H
+#define __SQUIRREL_H
+
+#include "../drivers/bluetooth/bluetooth.h"
 #include "../protocols/downlink/downlink.h"
-//#include "../protocols/uplink/uplink.h"
+#include "../protocols/uplink/uplink.h"
 
-
-	extern void squirrel_init(void); // Initialize, check for known devices, scan for new devices, start polling/accept pushed messages, output to display, respond to backend requests, log activity
-	//void __bluetooth_init();
-	
-#endif // end of SQUIRREL
+#endif
 
