fae84144f3c0eb8116e6718b1aad417cfba600e1
Salomon Sickert <sickert@in.tum.de>
2010-05-19 13:29:55 +0200

    Uplink extensions

--- protocols/uplink/uplink.h
+++ protocols/uplink/uplink.h
@@ -19,14 +19,12 @@ const int UPLINK_PACKET_LENGTH = 64;
  *
  * LIST:
  *
- * | OPCODE (1)  | PAGE (1) | -- (6)
- * | BD ADDR (6) | NUT_CLASS (1) | EXTENSION_CLASS_1 (1) | .... | TERMINATOR (1)
- * .....
- * | BD ADDR (6) | NUT_CLASS (1) | EXTENSION_CLASS_1 (1) | .... | TERMINATOR (1)
+ * | OPCODE (1)  | PAGE (1) | BD ADDR (6) |
+ * | NUT_CLASS (1) | EXTENSION_CLASS_1 (1) | .... | TERMINATOR (*) = INVALID
  *
  * LOG:
  *
- * | OPCODE (1) | -- (1) | BD ADDR (6)  |
+ * | OPCODE (1) | PAGE (1) | BD ADDR (6)  |
  * | EXTENSION_ID (1) | EXTENSION_CLASS (1) | LOG_TIME (4) | TIME_INTERVALL (2)
  * | VALUE1 (2) | .... 	| VALUE4 (2) |
  * .....
@@ -42,10 +40,43 @@ const int UPLINK_PACKET_LENGTH = 64;
  * | DATA   (4) | -- (52) |
  */
 
+struct uplink_payload_list {
+	uint8_t bt_address[6];
+	uint8_t nut_class;
+	uint8_t extension_class[55];
+};
+
+struct  uplink_payload_log {
+	uint8_t bt_address[6];
+	uint8_t extension_id;
+	uint8_t extension_class;
+	uint32_t log_time;
+	uint16_t time_intervall;
+	uint16_t values[24];
+};
+
+struct  uplink_payload_time {
+	uint32_t time;
+	uint8_t padding[58];
+};
+
+struct  uplink_payload_tunnel {
+	uint8_t bt_address[6];
+	uint8_t tunnel[4];
+	uint8_t padding[52];
+};
+
+union payload {
+	struct  uplink_payload_list;
+	struct  uplink_payload_log;
+	struct  uplink_payload_time;
+	struct  uplink_payload_tunnel;
+} payload;
+
 struct uplink_packet {
 	uint8_t opcode;
 	uint8_t id;
-	uint8_t value;
+	union  payload;
 } downlink_packet;
 
 
