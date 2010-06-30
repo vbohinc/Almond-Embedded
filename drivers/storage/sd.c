bed80c612d1338d44de1930ce3cb9c975fb4b935
Sean Labastile <sean.labastille@mytum.de>
2010-06-30 16:09:09 +0200

    Added missing headers

--- drivers/storage/sd.c
+++ drivers/storage/sd.c
@@ -1,4 +1,29 @@
-
+      ___                         ___           ___           ___          _____    
+     /  /\                       /__/\         /  /\         /__/\        /  /::\   
+    /  /::\                     |  |::\       /  /::\        \  \:\      /  /:/\:\  
+   /  /:/\:\    ___     ___     |  |:|:\     /  /:/\:\        \  \:\    /  /:/  \:\ 
+  /  /:/~/::\  /__/\   /  /\  __|__|:|\:\   /  /:/  \:\   _____\__\:\  /__/:/ \__\:|
+ /__/:/ /:/\:\ \  \:\ /  /:/ /__/::::| \:\ /__/:/ \__\:\ /__/::::::::\ \  \:\ /  /:/
+ \  \:\/:/__\/  \  \:\  /:/  \  \:\~~\__\/ \  \:\ /  /:/ \  \:\~~\~~\/  \  \:\  /:/ 
+  \  \::/        \  \:\/:/    \  \:\        \  \:\  /:/   \  \:\  ~~~    \  \:\/:/  
+   \  \:\         \  \::/      \  \:\        \  \:\/:/     \  \:\         \  \::/   
+    \  \:\         \__\/        \  \:\        \  \::/       \  \:\         \__\/    
+     \__\/                       \__\/         \__\/         \__\/                  
+      ___                       ___           ___           ___           ___           ___     
+     /  /\          ___        /  /\         /  /\         /  /\         /  /\         /  /\    
+    /  /:/_        /  /\      /  /::\       /  /::\       /  /::\       /  /:/_       /  /:/_   
+   /  /:/ /\      /  /:/     /  /:/\:\     /  /:/\:\     /  /:/\:\     /  /:/ /\     /  /:/ /\  
+  /  /:/ /::\    /  /:/     /  /:/  \:\   /  /:/~/:/    /  /:/~/::\   /  /:/_/::\   /  /:/ /:/_ 
+ /__/:/ /:/\:\  /  /::\    /__/:/ \__\:\ /__/:/ /:/___ /__/:/ /:/\:\ /__/:/__\/\:\ /__/:/ /:/ /\
+ \  \:\/:/~/:/ /__/:/\:\   \  \:\ /  /:/ \  \:\/:::::/ \  \:\/:/__\/ \  \:\ /~~/:/ \  \:\/:/ /:/
+  \  \::/ /:/  \__\/  \:\   \  \:\  /:/   \  \::/~~~~   \  \::/       \  \:\  /:/   \  \::/ /:/ 
+   \__\/ /:/        \  \:\   \  \:\/:/     \  \:\        \  \:\        \  \:\/:/     \  \:\/:/  
+     /__/:/          \__\/    \  \::/       \  \:\        \  \:\        \  \::/       \  \::/   
+     \__\/                     \__\/         \__\/         \__\/         \__\/         \__\/    
+/*
+sd.c - SD lives here
+*/
+#include ../spi/spi.h
 void sd_init() {
 	spi_init();
 	// Place SD Card into Idle State
@@ -43,15 +68,25 @@ boolean sd_send_command(uint8_t command_nr, uint8_t *arguments) {
 	switch (command_nr) {
 		case CMD0:
 			// Static CMD0. As the card is still in SD mode a valid CRC is required
-			sd_buffer = 0x400000000095;
+			//sd_buffer = 0x400000000095;
+			sd_buffer[0] = 0x40;
+			sd_buffer[1] = sd_buffer[2] = sd_buffer[3] = sd_buffer[4] = 0x00;
+			sd_buffer[5] = 0x95;
 			break;
 		case CMD8:
 			// Static CMD8. Ask the card if it likes tasty 2.7-3.6V
-			sd_buffer = 0x480000010001;
+			//sd_buffer = 0x480000010001;
+			sd_buffer[0] = 0x48;
+			sd_buffer[1] = sd_buffer[2] = sd_buffer[4] = 0x00;
+			sd_buffer[3] = sd_buffer[5] 0x01;
 			break;
 		case CMD16:
 			// Static CMD16. Sets the block size to 32 bytes.
-			sd_buffer = 0x500000002001;
+			//sd_buffer = 0x500000002001;
+			sd_buffer[0] = 0x50;
+			sd_buffer[1] = sd_buffer[2] = sd_buffer[3] = 0x00;
+			sd_buffer[4] = 0x20;
+			sd_buffer[5] 0x01;
 			break;
 		case CMD17:
 			// Requests a single block beginning at the address passed (4 Bytes)
@@ -69,15 +104,25 @@ boolean sd_send_command(uint8_t command_nr, uint8_t *arguments) {
 			break;
 		case CMD55:
 			// Static CMD55. Informs the card the next command will be an 'A'-Command (Application Specific)
-			sd_buffer = 0x770000000001;
+			//sd_buffer = 0x770000000001;
+			sd_buffer[0] = 0x77;
+			sd_buffer[1] = sd_buffer[2] = sd_buffer[3] = sd_buffer[4] = 0x00;
+			sd_buffer[5] = 0x01;
 			break;
 		case ACMD41:
 			// Static ACMD41. Initialize the card. Inform it we don't support High Capacity.
-			sd_buffer = 0x696000000001;
+			//sd_buffer = 0x696000000001;
+			sd_buffer[0] = 0x69;
+			sd_buffer[1] = 0x60;
+			sd_buffer[2] = sd_buffer[3] = sd_buffer[4] = 0x00;
+			sd_buffer[5] = 0x01;
 			break;
 		case CMD58:
-			// Static CMD58. Get OCR. Returns the Card Capacity.
-			sd_buffer = 0x790000000001;
+			// Static CMD58. Get OCR. (Operating Conditions Register) Returns the Card Capacity. 
+			//sd_buffer = 0x790000000001;
+			sd_buffer[0] = 0x79;
+			sd_buffer[1] = sd_buffer[2] = sd_buffer[3] = sd_buffer[4] = 0x00;
+			sd_buffer[5] = 0x01;
 			break;
 		default:
 			return false;
@@ -118,7 +163,7 @@ void sd_write_block(uint8_t *addr, uint8_t *write_buffer) {
 			for (int i = 0; i < 512; i++) {
 				spi_send_byte(write_buffer[i]);
 			}
-			uint8_t crc[2] = 0xFFFF;
+			uint8_t crc[2] = {0xFF, 0xFF}; //0xFFFF;
 			spi_send_byte(crc[0]);
 			spi_send_byte(crc[1]);
 		}
