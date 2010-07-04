0addc5ac39d37886d4a961a27c8e4de05ef13ccc
Sean Labastile <sean.labastille@mytum.de>
2010-07-04 19:12:50 +0200

    fixed silly mistakes (UINT8_T!)

--- spi_test/spi_test.c
+++ spi_test/spi_test.c
@@ -1,8 +1,8 @@
 #include "../drivers/storage/sd.h"
 
-int[] bytes = {0xAA, 0xAA, 0xAA};
+uint8_t bytes[3] = {0xAA, 0xAA, 0xAA};
 
-int main(void) {
+uint8_t main(void) {
 	sd_init();
 	sd_write_bytes(0, bytes, 3);
 	return 0;
