d73b514dc7695306b6cde0fb1ba5a7a2a302fead
Maximilian Karl <marcaxi@gmx.de>
2010-08-07 14:31:58 +0200

    i2c fixed, bmp085 fixed, spi/sd changed

--- nuts/weatherstation/main.c
+++ nuts/weatherstation/main.c
@@ -49,6 +49,38 @@
 #define LED1_PIN 7
 #define LED2_PIN 0
 
+uint16_t read_adc(uint8_t pin)
+{
+	// wandlung starten
+	ADCSRA |= (1<<ADSC);
+	// warten bis ergebniss
+	while(ADCSRA & (1<<ADSC));
+	
+	return ADCW;
+}
+
+void init_adc(uint8_t pin, uint8_t factor)
+{
+	// Den ADC aktivieren und Teilungsfaktor auf 64 stellen
+	ADCSRA = (1<<ADEN) | factor;
+
+	// eingangspin waehlen
+	ADMUX = pin;
+
+	// referenzspannung waehlen
+	ADMUX |= (1<<REFS1) | (1<<REFS0); // interne referenzspannung (also 2,56 V)
+	//ADMUX |= (1<<REFS0); // avcc als referenzspannung
+	//ADMUX |= 0;// aref als referenzspannung
+
+	// erstes mal auslesen, dummyreadout
+	read_adc(pin);
+}
+
+void close_adc()
+{
+	ADCSRA &= ~(1<<ADEN);
+}
+
 
 const uint8_t class_id_nut = WEATHERSTATION;
 const uint8_t class_id_extensions[] = {TEMPERATURE, PRESSURE, LED};
@@ -56,7 +88,7 @@ const uint8_t class_id_extensions_length = 3;
 
 int main (void)
 {
-	LED1_DDR |= 0xFF;
+	//LED1_DDR |= 0xFF;
 
 	//LED2_DDR |= 0xFF;
 	/*LED1_PORT |= (1<<LED1_PIN);
@@ -68,28 +100,53 @@ int main (void)
 
 
 	//init sleeping
-	init_timer();
+	//init_timer();
 	error_init();
 	//debug_pgm(PSTR("#NUT#"));
 	//start bluetooth
-	bluetooth_init(downlink_bluetooth_callback_handler);
-	sei();
-	bluetooth_test_connection(4); //random number
-	bluetooth_set_as_slave();
+	//bluetooth_init(downlink_bluetooth_callback_handler);
+	//sei();
+	//bluetooth_test_connection(4); //random number
+	//bluetooth_set_as_slave();
 	//initialize sensors
-	//init_bmp085_sensor();
+        //_delay_ms(1000);
+	init_bmp085_sensor();
 
 
 	//debug_pgm(PSTR("WTPG"));
 	//mainloop
 	while(1)
 	{
+//byte_to_hex(bmp085_get_data().temprature>>8);
+
+/*
+// bmp testauisgabe
+		bmp_data_t d = bmp085_get_data();
+	debug_pgm(PSTR("temp"));	
+byte_to_hex(d.temprature>>8);
+byte_to_hex(d.temprature);
+	debug_pgm(PSTR("press"));
+byte_to_hex(d.pressure>>8);
+byte_to_hex(d.pressure);*/
+
+debug_pgm(PSTR("lux"));
+init_adc(0,(1<<ADPS1) | (1<<ADPS0));
+uint16_t out = read_adc(0);
+byte_to_hex(out>>8);
+byte_to_hex((uint8_t)out);
+
+
+debug_pgm(PSTR("hum"));
+out = read_adc(1);
+byte_to_hex(out>>8);
+byte_to_hex((uint8_t)out);
+
 		//activate bluetooth
 		//TODO make meassurements
 		//TODO check variable for bluetooth
 		//TODO sleep again if no connection is recieved
 		//process data
-		bluetooth_process_data();
+		//bluetooth_process_data();
 
 		// start_sleep(4); //TODO use a variable, instead of random number
 		//_delay_ms(1000);
