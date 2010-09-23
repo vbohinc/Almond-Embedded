f9773310a322daa43568840303414e35bd1f8a06
Salomon Sickert <sickert@in.tum.de>
2010-09-24 00:04:51 +0200

    Neue Bluetooth implementierung: * neue fifos * sauberes fifo handling * einfacherer sauberer  code * abschaffung der callbacks
    Code ist noch WIP

--- shared/fifo.c
+++ shared/fifo.c
@@ -1,52 +1,126 @@
-/*
-      ___                         ___           ___           ___          _____    
-     /  /\                       /__/\         /  /\         /__/\        /  /::\   
-    /  /::\                     |  |::\       /  /::\        \  \:\      /  /:/\:\  
-   /  /:/\:\    ___     ___     |  |:|:\     /  /:/\:\        \  \:\    /  /:/  \:\ 
-  /  /:/~/::\  /__/\   /  /\  __|__|:|\:\   /  /:/  \:\   _____\__\:\  /__/:/ \__\:|
- /__/:/ /:/\:\ \  \:\ /  /:/ /__/::::| \:\ /__/:/ \__\:\ /__/::::::::\ \  \:\ /  /:/
- \  \:\/:/__\/  \  \:\  /:/  \  \:\~~\__\/ \  \:\ /  /:/ \  \:\~~\~~\/  \  \:\  /:/ 
-  \  \::/        \  \:\/:/    \  \:\        \  \:\  /:/   \  \:\  ~~~    \  \:\/:/  
-   \  \:\         \  \::/      \  \:\        \  \:\/:/     \  \:\         \  \::/   
-    \  \:\         \__\/        \  \:\        \  \::/       \  \:\         \__\/    
-     \__\/                       \__\/         \__\/         \__\/                  
-      ___           ___           ___           ___           ___           ___     
-     /  /\         /  /\         /__/\         /__/\         /  /\         /__/\    
-    /  /:/        /  /::\       |  |::\       |  |::\       /  /::\        \  \:\   
-   /  /:/        /  /:/\:\      |  |:|:\      |  |:|:\     /  /:/\:\        \  \:\  
-  /  /:/  ___   /  /:/  \:\   __|__|:|\:\   __|__|:|\:\   /  /:/  \:\   _____\__\:\ 
- /__/:/  /  /\ /__/:/ \__\:\ /__/::::| \:\ /__/::::| \:\ /__/:/ \__\:\ /__/::::::::\
- \  \:\ /  /:/ \  \:\ /  /:/ \  \:\~~\__\/ \  \:\~~\__\/ \  \:\ /  /:/ \  \:\~~\~~\/
-  \  \:\  /:/   \  \:\  /:/   \  \:\        \  \:\        \  \:\  /:/   \  \:\  ~~~ 
-   \  \:\/:/     \  \:\/:/     \  \:\        \  \:\        \  \:\/:/     \  \:\     
-    \  \::/       \  \::/       \  \:\        \  \:\        \  \::/       \  \:\    
-     \__\/         \__\/         \__\/         \__\/         \__\/         \__\/    
-
-*/
 #include "fifo.h"
 
-void fifo_init (fifo_t *f, uint8_t *buffer, const uint8_t size)
+
+void
+fifo_init (fifo_t* fifo, uint8_t* buffer, uint8_t size)
 {
-	f->count = 0;
-	f->pread = f->pwrite = buffer;
-	f->read2end = f->write2end = f->size = size;
+  fifo->size = size;
+  fifo->buffer = buffer;
+  fifo->head = 0;
+  fifo->count = 0;
 }
 
-uint8_t fifo_put (fifo_t *f, const uint8_t data)
+bool
+fifo_is_empty(fifo_t* fifo)
 {
-	return _inline_fifo_put (f, data);
+  return (fifo->count == 0);
 }
 
-uint8_t fifo_get_wait (fifo_t *f)
+bool 
+fifo_is_full(fifo_t* fifo)
 {
-	while (!f->count);
-	
-	return _inline_fifo_get (f);	
+  return (fifo->size - fifo->count <= 0);
 }
 
-int16_t fifo_get_nowait (fifo_t *f)
+bool 
+fifo_read(fifo_t* fifo, uint8_t* data, uint8_t length)
 {
-	if (!f->count)		return -1;
-		
-	return (uint8_t) _inline_fifo_get (f);
+  if (!fifo_is_empty(fifo))
+    {
+      if (length < fifo->count)
+        {
+          uint8_t* output = data;
+          for (uint8_t i=0; i<length; i++)
+            {
+              fifo->head = ((fifo->head + i)%fifo->size);
+              uint8_t* head = fifo->buffer + fifo->head;
+              *output = *head;
+              fifo->count--;
+            }
+          return true;
+        }
+    }
+  return false;
+}
+
+
+bool 
+fifo_read_c(fifo_t* fifo, char* data)
+{
+  if (!fifo_is_empty (fifo))
+    {
+      uint8_t* head = fifo->buffer + fifo->head;
+      *data = (char) *head;
+      fifo->head = ((fifo->head + 1)%fifo->size);
+      fifo->count--;
+      return true;
+    }
+  return false;
+}
+
+bool 
+fifo_write_c(fifo_t* fifo, char* data)
+{
+  if (!fifo_is_full(fifo))
+    {
+      uint8_t* end = fifo->buffer + ((fifo->head + fifo->count)%fifo->size);
+      *end = (uint8_t) *data;
+      fifo->count++;
+      return true;
+    }
+  return false;
+}
+
+bool 
+fifo_clear(fifo_t* fifo)
+{
+  fifo->count = 0;
+  fifo->head = 0;
+  return true;
+}
+
+bool 
+fifo_write_p(fifo_t* fifo, prog_char* progmem)
+{
+  if (!fifo_is_full(fifo))
+    {
+      uint8_t i = 0;
+      uint8_t* end;
+      while (pgm_read_byte(progmem+i) != 0)
+        {
+          if (fifo_is_full(fifo))
+            return false;
+          end = fifo->buffer + ((fifo->head + fifo->count)%fifo->size);
+          *end = pgm_read_byte(progmem+i);
+          fifo->count++;
+          i++;
+        }
+      return true;
+    }
+  return false;
+}
+
+bool 
+fifo_cmp_pgm(fifo_t* fifo, prog_char* progmem)
+{
+  if (!fifo_is_empty(fifo))
+    {
+      prog_char* pgm = progmem;
+      uint8_t i = 0;
+      uint8_t head = 0;
+      uint8_t* head_p;
+      uint8_t count = fifo->count;
+      while (pgm_read_byte(pgm) != 0)
+        {
+          head = ((fifo->head + i)%fifo->size);
+          head_p = fifo->buffer + head;
+          if (count <= 0 || *head_p != (uint8_t) *pgm)
+            return false;
+          pgm++;
+          i++;
+          count--;
+        }
+      return true;
+    }
+  return false;
 }
--- shared/fifo.h
+++ shared/fifo.h
@@ -1,153 +1,38 @@
-/*
-      ___                         ___           ___           ___          _____    
-     /  /\                       /__/\         /  /\         /__/\        /  /::\   
-    /  /::\                     |  |::\       /  /::\        \  \:\      /  /:/\:\  
-   /  /:/\:\    ___     ___     |  |:|:\     /  /:/\:\        \  \:\    /  /:/  \:\ 
-  /  /:/~/::\  /__/\   /  /\  __|__|:|\:\   /  /:/  \:\   _____\__\:\  /__/:/ \__\:|
- /__/:/ /:/\:\ \  \:\ /  /:/ /__/::::| \:\ /__/:/ \__\:\ /__/::::::::\ \  \:\ /  /:/
- \  \:\/:/__\/  \  \:\  /:/  \  \:\~~\__\/ \  \:\ /  /:/ \  \:\~~\~~\/  \  \:\  /:/ 
-  \  \::/        \  \:\/:/    \  \:\        \  \:\  /:/   \  \:\  ~~~    \  \:\/:/  
-   \  \:\         \  \::/      \  \:\        \  \:\/:/     \  \:\         \  \::/   
-    \  \:\         \__\/        \  \:\        \  \::/       \  \:\         \__\/    
-     \__\/                       \__\/         \__\/         \__\/                  
-      ___           ___           ___           ___           ___           ___     
-     /  /\         /  /\         /__/\         /__/\         /  /\         /__/\    
-    /  /:/        /  /::\       |  |::\       |  |::\       /  /::\        \  \:\   
-   /  /:/        /  /:/\:\      |  |:|:\      |  |:|:\     /  /:/\:\        \  \:\  
-  /  /:/  ___   /  /:/  \:\   __|__|:|\:\   __|__|:|\:\   /  /:/  \:\   _____\__\:\ 
- /__/:/  /  /\ /__/:/ \__\:\ /__/::::| \:\ /__/::::| \:\ /__/:/ \__\:\ /__/::::::::\
- \  \:\ /  /:/ \  \:\ /  /:/ \  \:\~~\__\/ \  \:\~~\__\/ \  \:\ /  /:/ \  \:\~~\~~\/
-  \  \:\  /:/   \  \:\  /:/   \  \:\        \  \:\        \  \:\  /:/   \  \:\  ~~~ 
-   \  \:\/:/     \  \:\/:/     \  \:\        \  \:\        \  \:\/:/     \  \:\     
-    \  \::/       \  \::/       \  \:\        \  \:\        \  \::/       \  \:\    
-     \__\/         \__\/         \__\/         \__\/         \__\/         \__\/    
-
-
-*
+/**
 * @file fifo.h
-* @author Stefan Profanter
-* @date 01.05.2010
+* @author Pascal Schnurr
+* @date 23.09.2010
 *
-* This file contains a representation of a FIFO list. The list can store only uint8_t data.
+*  This is the new fifo implementation for the blueooth rewrite.
 *
 */
+#include <avr/pgmspace.h>
+#include <common.h>
+
 #ifndef _FIFO_H_
 #define _FIFO_H_
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-/**
- * The FIFO.
- * Stores the data.
- */
 typedef struct
 {
-	/**
-	 * Member of the struct
-	 */
-	uint8_t volatile count;       /*! Number of bytes in fifo */
-	uint8_t size;                 /*! Size of fifo (buffer) */
-	uint8_t *pread;               /*! read pointer */
-	uint8_t *pwrite;              /*! write pointer */
-	uint8_t read2end;             /*! number of bytes to overflow of read pointer */
-	uint8_t write2end;  		  /*! number of bytes to overflow of write pointer */
+  uint8_t size;
+  uint8_t count;
+  uint8_t* buffer;
+  uint8_t head;
 } fifo_t;
 
-/**
- * Initialize a new FIFO.
- * Initializes a new FIFO with the given buffer and size.
- * @param fifo FIFO to initialize
- * @param buf The already initialized buffer array for the fifo with the size of 'size'
- * @param size Size of the Buffer.
- */
-extern void fifo_init (fifo_t* fifo, uint8_t* buf, const uint8_t size);
+void fifo_init(fifo_t* new_fifo,uint8_t* buffer, uint8_t size);
 
-/**
- * Put a byte into the fifo.
- * @param fifo Fifo to put byte into
- * @param data The data-byte to store
- */
-extern uint8_t fifo_put (fifo_t* fifo, const uint8_t data);
+bool fifo_is_empty (fifo_t* fifo);
+bool fifo_is_full (fifo_t* fifo);
 
+bool fifo_cmp_pgm (fifo_t* fifo, prog_char* programm);
 
-/**
- * Get the first byte. If no byte available, wait until one will be put.
- * @param fifo Fifo to read from
- */
-extern uint8_t fifo_get_wait (fifo_t* fifo);
+bool fifo_read (fifo_t* fifo, uint8_t* data, uint8_t length);
+bool fifo_read_c (fifo_t* fifo, char* data);
 
-/**
- * Get the first byte. If no byte available, returns -1.
- * @param fifo Fifo to read from
- */
-extern int16_t fifo_get_nowait (fifo_t* fifo);
+bool fifo_write_c (fifo_t* fifo, char* data);
+bool fifo_write_pgm (fifo_t* fifo, prog_char* programm);
 
-/**
- * Puts a byte into the fifo. Is called by fifo_put.
- * @param fifo Fifo to put byte into
- * @param data The data-byte to store
- */
-static inline uint8_t _inline_fifo_put (fifo_t *fifo, const uint8_t data)
-{
-	if (fifo->count >= fifo->size)
-		return 0;
-
-	uint8_t sreg = SREG;
-	cli();
-
-	uint8_t * pwrite = fifo->pwrite;
-	
-	*(pwrite++) = data;
-	
-	uint8_t write2end = fifo->write2end;
-	
-	if (--write2end == 0)
-	{
-		write2end = fifo->size;
-		pwrite -= write2end;
-	}
-	
-	fifo->write2end = write2end;
-	fifo->pwrite = pwrite;
-
-	fifo->count++;
-
-	SREG = sreg;
-	
-	return 1;
-}
-
-/**
- * Reads a byte from the fifo. Is called by fifo_get_nowait and fifo_get_wait.
- * @param fifo Fifo to read from
- */
-static inline uint8_t _inline_fifo_get (fifo_t *f)
-{
-
-	uint8_t sreg = SREG;
-	cli();
-
-	uint8_t *pread = f->pread;
-	uint8_t data = *(pread++);
-	uint8_t read2end = f->read2end;
-	
-	if (--read2end == 0)
-	{
-		read2end = f->size;
-		pread -= read2end;
-	}
-	
-	f->pread = pread;
-	f->read2end = read2end;
-	
-
-
-	f->count--;
-
-	SREG = sreg;
-	
-	return data;
-}
+bool fifo_clear (fifo_t* fifo);
 
 #endif /* _FIFO_H_ */
