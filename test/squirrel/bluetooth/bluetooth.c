b375c8eae20a7287d86b857b8e1162a54dced52d
Linus Lotz <linus.lotz@mytum.de>
2010-10-19 09:42:18 +0200

    some changes

--- test/squirrel/bluetooth/bluetooth.c
+++ test/squirrel/bluetooth/bluetooth.c
@@ -5,16 +5,17 @@
 static bool
 discover_callback(const char *name, const uint8_t *address)
 {
-	//display address
+	display_print(name);
 	return true;
 }
 
 int main(void)
 {
+	display_init();
 	bt_init();
 	bt_set_mode(BLUETOOTH_MASTER);
 	char[8][6] result;
-	bt_discover(result, NULL);
-
+	display_print("discover:\n");
+	bt_discover(result, discover_callback);
 }
 
