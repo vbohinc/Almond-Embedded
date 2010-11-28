20d9d17b63965e1f6340ac28dc3a5a734a6c67e0
Pro <stefan.profanter@mytum.de>
2010-11-28 13:27:58 +0100

    Removed some useless test files

--- test/build_display_test.sh
+++
@@ -1,2 +0,0 @@
-gcc -I/opt/local/include/SDL/ -I../shared/ ./pong.c ./squirrel/display/display_test.c ../drivers/gui/display_gui.c ../drivers/gui/display_data.c ../drivers/display/display.c ../drivers/gui/display_draw.c -std=c99 -DX86 `sdl-config --cflags --libs` -lSDL_gfx -lm -o test 
-./test
