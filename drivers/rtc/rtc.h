35e9949b78042000ecd76307ec9a99a68471f4d4
Linus Lotz <linus.lotz@mytum.de>
2010-12-04 16:54:12 +0100

    new rtc

--- drivers/rtc/rtc.h
+++ drivers/rtc/rtc.h
@@ -8,23 +8,10 @@
 
 #include "common.h"
 
-struct __time_struct
-{
-    uint8_t  sec;   // Seconds      [0-59] (no leap second!)
-    uint8_t  min;   // Minutes      [0-59]
-    uint8_t  hour;  // Hour         [0-23]
-    uint8_t  mday;  // Day in Month [1-31]
-    uint8_t  month; // Month        [1-12]
-    uint16_t year;  // Year - 1900
-    uint8_t  wday;  // Day of Week  [0-6]
-};
-
-typedef struct __time_struct  time_t;
-
 void rtc_init(void);
 
-time_t get_time (void);
+uint32_t get_time (void);
 
-void set_time (time_t*);
+void set_time (uint32_t);
 
 #endif //__RTC_H_
